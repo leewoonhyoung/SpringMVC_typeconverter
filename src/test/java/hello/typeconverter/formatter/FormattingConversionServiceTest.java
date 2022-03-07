048c92bb6c88cf23cada104de81ec998b3755f3c
leewoonhyoung <wh3619@gmail.com>
2022-03-07 12:09:30 +0900

    fileUploader program 2022-03-07

--- src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
+++ src/test/java/hello/typeconverter/formatter/FormattingConversionServiceTest.java
@@ -2,6 +2,8 @@ package hello.typeconverter.formatter;
 
 import hello.typeconverter.converter.IpPortToStringConverter;
 import hello.typeconverter.converter.StringToIntegerConverter;
+import hello.typeconverter.type.IpPort;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.format.support.DefaultFormattingConversionService;
@@ -15,5 +17,14 @@ public class FormattingConversionServiceTest {
         conversionService.addConverter(new StringToIntegerConverter());
         conversionService.addConverter(new IpPortToStringConverter());
         conversionService.addFormatter(new MyNumberFormatter());
+
+        IpPort ipPort = conversionService.convert("127.0.0.1:8080", IpPort.class);
+        Assertions.assertThat(ipPort).isEqualTo(new IpPort("127.0.0.1", 8080));
+
+        //포맷터 사용
+        Assertions.assertThat(conversionService.convert(1000, String.class)).isEqualTo("1,000");
+        Assertions.assertThat(conversionService.convert("1,000", Long.class)).isEqualTo(1000L);
     }
+
 }
+
